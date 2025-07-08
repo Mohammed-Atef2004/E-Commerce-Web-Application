9a9e394ca48e64602d26880cbd56219271b4a656
Mohammed Atef <muhamedatef@gmail.com>
2025-07-09 02:37:26 +0300

    create categories table in the database

---
+++ myShop.Web/Data/ApplicationDbContext.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using myShop.Web.Models;
+
+namespace myShop.Web.Data
+{
+    public class ApplicationDbContext:DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options):base(options)
+        {
+            
+        }
+        public DbSet<Category>Categories { get; set; }
+        //protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        //{
+        //    var config = new ConfigurationBuilder().AddJsonFile("appsettings.Development.json").Build();
+        //    var connectionString = config.GetSection("ConnectionStrings").Value;
+
+        //    optionsBuilder.UseSqlServer(connectionString);
+        //}
+    }
+}
