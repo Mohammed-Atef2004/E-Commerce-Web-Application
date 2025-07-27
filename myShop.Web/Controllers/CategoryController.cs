38e10f8e7ea37955165b3107b79cdbe9318a36e2
Mohammed Atef <muhamedatef@gmail.com>
2025-07-27 14:26:56 +0300

    Adding Category Module

---
+++ myShop.Web/Controllers/CategoryController.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using myShop.Web.Data;
+using myShop.Web.Models;
+
+namespace myShop.Web.Controllers
+{
+    public class CategoryController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+        public CategoryController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+        public IActionResult Index()
+        {
+            var categories = _context.Categories.ToList();
+            return View(categories);
+        }
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(Category category)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Categories.Add(category);
+                _context.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(category);
+        }
+        [HttpGet]
+        public IActionResult Edit(int? Id)
+        {
+            if (Id == null || Id == 0) 
+            {
+                return NotFound();
+            }
+            var category = _context.Categories.FirstOrDefault(c => c.Id == Id);
+            return View(category);
+        }
+        [HttpPost]
+        public IActionResult Edit(Category category)
+        {
+            if(ModelState.IsValid)
+            {
+                _context.Categories.Update(category);
+                _context.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(category);
+        }
+    }
+}
